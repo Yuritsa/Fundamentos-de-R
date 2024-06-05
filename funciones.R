4cdfc1dc7acc5431b4d93fa257baa038067025f1
Yuritsa Páez <53491820+Yuritsa@users.noreply.github.com>
2024-06-05 10:40:57 -0600

    Add files via upload

---
+++ funciones.R
@@ -0,0 +1,20 @@
+#1. paso: Definir la función y colocar los argumnetos de entrada
+# 2. paso: Cuerpo de la función : que deseas hacer, la operación a realizar
+# 3 paso: regrese la función, que nos devuelva el resultado #return
+# 4. llamar la función e imprimir el resultado
+
+#Sumar dos números
+
+sumar <- function(a, b) {
+  
+  resultado <- a+b
+  return(resultado)
+}
+
+#Usar la función
+
+resultado_suma <- sumar(5,4)
+print(resultado_suma)
+
+
+
